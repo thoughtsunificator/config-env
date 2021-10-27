f7fef13a0d2120d0f7da4b134f96bcdb48cadd53
thoughtsunificator <oss@thoughtsunificator.me>
2021-10-27 16:36:13 +0100

    cleanup

---
+++ test/index.js
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import config, { createConfig } from '../index.js'
+
+describe("Observable", function () {
+
+	it("config", function() {
+
+	})
+
+	it("createConfig", function() {
+
+	})
+
+})
