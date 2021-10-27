0599c8d29ec4ca8a0d436a8f256f5c7a29856766
thoughtsunificator <oss@thoughtsunificator.me>
2021-10-27 20:27:56 +0100

    added simple test cases

--- test/index.js
+++ test/index.js
@@ -1,14 +1,18 @@
 import assert from 'assert'
 import config, { createConfig } from '../index.js'
 
-describe("Observable", function () {
+describe("index", function () {
 
 	it("config", function() {
-
+		assert.deepEqual(config, {})
 	})
 
 	it("createConfig", function() {
-
+		assert.deepEqual(createConfig("./test/examples/.env.json", "./test/examples/config.json"), {
+			"MY_TITLE": "HELLO WORLD",
+			"FOO": true,
+			"BAR": false
+		})
 	})
 
 })
